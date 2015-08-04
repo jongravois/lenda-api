3d1d23087239e68f2d10a944420784806752145e
Jon Gravois <jongravois@gmail.com>
2015-08-04 00:16:29 -0500

    Fixed QQBSync Model "

--- app/Qbsync.php
+++ app/Qbsync.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Qbsync extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'qbsync';
+    protected $fillable = [''];
+
+    /* CASTING */
+    protected $casts = [];
+    /* CASTING */
+
+    /* RELATIONSHIPS */
+    /* RELATIONSHIPS */
+
+    /* METHODS */
+    /* METHODS */
 }
