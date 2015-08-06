b5596e1d77f433e0ab1f0047af3254d180ed52c1
Jon Gravois <jongravois@gmail.com>
2015-08-06 14:07:13 -0500

    Summary and Underwriting

--- app/Applicant.php
+++ app/Applicant.php
@@ -10,6 +10,9 @@ class Applicant extends Model
     protected $fillable = ['grade', 'loc_id', 'entity_id', 'farmer_id', 'applicant.financials', 'ssn', 'email', 'dob', 'address', 'phone', 'city', 'state_id', 'zip', 'spouse', 'spouse_ssn'];
 
     /* RELATIONSHIPS */
+    public function entity() {
+        return $this->belongsTo('App\Entitytype', 'entity_id');
+    }
     public function fins() {
         return $this->hasOne('App\Applicantfinancial');
     }
