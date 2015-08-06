a5a31519a260bb11759b4a303037c96932ee14e2
Jon Gravois <jongravois@gmail.com>
2015-08-06 02:38:27 -0500

    crop datatypes

--- app/Crop.php
+++ app/Crop.php
@@ -9,6 +9,18 @@ class Crop extends Model
     protected $table = 'crops';
     protected $fillable = ['crop', 'name', 'sort_order', 'tea', 'arm_default_price', 'arm_default_ins_price', 'arm_default_yield', 'measurement', 'rebate_measurement'];
 
+    /* CASTS */
+    protected $casts = [
+        'id' => 'integer',
+        'sort_order' => 'integer',
+        'tea' => 'double',
+        'arm_default_price' => 'double',
+        'arm_default_ins_price' => 'double',
+        'arm_default_yield' => 'double'
+
+    ];
+    /* CASTS */
+
     /* RELATIONSHIPS */
     /* RELATIONSHIPS */
 
