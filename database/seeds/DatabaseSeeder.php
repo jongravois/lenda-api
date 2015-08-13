c08d3d371cbf455c9d93975de97672ec96410725
Jon Gravois <jongravois@gmail.com>
2015-08-13 03:37:36 -0500

    Added Crop Hail to Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -40,6 +40,7 @@ class DatabaseSeeder extends Seeder
             'FarmunitsTableSeeder',
             'GlobvarsTableSeeder',
             'GuarantorsTableSeeder',
+            'InscrophailsTableSeeder',
             'InsoptsTableSeeder',
             'IndirectcropincomesTableSeeder',
             'InspolsTableSeeder',
