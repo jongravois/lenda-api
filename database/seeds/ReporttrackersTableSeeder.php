9c678bd2b32b3563443a551ad65b892052af5ca4
petrovitch <kennthompson@gmail.com>
2015-08-20 11:26:28 -0500

    nothing

--- database/seeds/ReporttrackersTableSeeder.php
+++ database/seeds/ReporttrackersTableSeeder.php
@@ -13,20 +13,22 @@ class ReporttrackersTableSeeder extends Seeder
     {
         DB::table('reporttrackers')->delete();
 
-        $users = User::all();
-        $reports = Report::all();
+        $users = User::where('app', 'staff')->where('role_id', '!=', 20)->get();
+        $reports = Report::where('is_required', 1)->get();
 
-        for($u=3; $u<count($users); $u++) {
-            for($r=1; $r<count($reports); $r++){
+        for ($u = 3; $u < count($users); $u++) {
+            for ($r = 1; $r < count($reports); $r++) {
                 Reporttracker::create([
                     'report_id' => $r,
                     'user_id' => $u,
-                    'cnt_warned' => rand(0,15),
-                    'cnt_alerted' => rand(0,15),
-                    'made_required' => Carbon::now()->subDay(rand(11,140)),
-                    'last_acknowledged' => Carbon::now()->subDay(rand(0,10))
+                    'cnt_warned' => rand(0, 15),
+                    'cnt_alerted' => rand(0, 15),
+                    'made_required' => Carbon::now()->subDay(rand(11, 140)),
+                    'last_acknowledged' => Carbon::now()->subDay(rand(0, 10))
                 ]);
             }
         }
     }
 }
+
+
