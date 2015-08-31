4a2e16c0fba8e3c3c2b42d590f16970ad767b121
Jonathan Gravois <jongravois@gmail.com>
2015-08-31 16:22:44 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
     'timezone' => 'UTC',
     'locale' => 'en',
     'fallback_locale' => 'en',
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'SomeRandomStringWith32Characters'),
     'cipher' => 'AES-256-CBC',
     'log' => 'single',
     'providers' => [
